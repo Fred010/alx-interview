c5c1349be18a98806205abeb0f5ec3837e00814e
Fred010 <fjbaafi18@gmail.com>
2024-10-07 12:20:50 +0000

    Pascal's theorem practice

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""
+A module for working with Pascal's triangle.
+"""
 
 
 def pascal_triangle(n):
-    '''Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
-    '''
+    """
+    Generates Pascal's Triangle up to n rows.
+
+    Args:
+    n: The number of rows in the triangle - int
+
+    Returns:
+    A list of lists containing the Pascal's Triangle.
+    Returns an empty list if n <= 0.
+    """
+
     triangle = []
     if type(n) is not int or n <= 0:
                 return triangle
@@ -14,5 +23,8 @@ def pascal_triangle(n):
         if row == 0:
             triangle.append([1])
         else:
-            triangle.append([1] + [triangle[row-1][i] + triangle[row-1][i+1] for i in range(row-1)] + [1])
+            triangle.append(
+                    [1] +
+                    [triangle[row-1][i] + triangle[row-1][i+1] for i in range(
+                        row-1)] + [1])
     return triangle
