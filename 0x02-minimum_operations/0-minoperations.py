9bdc63fd4b4f77966fbbdfdabfe5279b5a485d07
Fred010 <fjbaafi18@gmail.com>
2024-10-16 03:06:31 +0000

    Minimum Operations task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
-'''
- Method that calculates the fewest number of operations,
- needed to result in exactly n H characters in the file.
-'''
+""" Module for 0-minoperations"""
 
 
 def minOperations(n):
-    '''Computes the fewest number of operations needed to result
-    in exactly n H characters.
-    '''
-    if n <= 0:
+    """
+    minOperations
+
+    Gets the fewest number of operations needed to
+    result in exactly n 'H' characters.
+
+    Algorithm:
+
+    Starting with root = 2, find the smallest root such
+    that n is evenly divisible by root.
+    For each such root, the number of even divisions by
+    root is the total number of operations
+    needed to reach n.
+
+    Args:
+        n (int): The target number of 'H' characters.
+
+    Returns:
+        int: The fewest number of operations needed to reach n 'H' characters.
+
+    """
+    # all outputs should be at least 2 char: (min, Copy All => Paste)
+    if n < 2:
         return 0
-    operations = 0
-    i = 2
-    while i * i <= n:
-        while n % i == 0:
-            operations += i
-            n /= i
-        i += 1
-    if n > 1:
-        operations += n
-    return operations
+    ops, root = 0, 2
+    while root <= n:
+        # if n evenly divides by root
+        if n % root == 0:
+            # total even-divisions by root = total operations
+            ops += root
+            # set n to the remainder
+            n = n // root
+            # reduce root to find remaining smaller vals that evenly-divide n
+            root -= 1
+        # increment root until it evenly-divides n
+        root += 1
+    return ops
